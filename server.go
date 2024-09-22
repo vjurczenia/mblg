b6d7a4c7624915d1b3b1b17540e3adc46c90a936
Victor Jurczenia <vjurczenia@gmail.com>
2024-09-22 11:25:55 +0000

    Enable CORS

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -62,5 +63,6 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/", index)
+	r.Use(cors.Default())
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
